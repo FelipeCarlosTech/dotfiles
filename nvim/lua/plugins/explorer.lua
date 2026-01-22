38e533826cdb1ee899c800c34625bf636f879c01
Felipe Carlos <felipecarlostech@gmail.com>
2026-01-22 14:58:18 -0500

    Fix hidden files config for neo-tree, snacks and telescope

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -6,24 +6,43 @@ return {
     opts = {
       filesystem = {
         filtered_items = {
-          visible = true, -- Show hidden files
+          visible = true,
           hide_dotfiles = false,
           hide_gitignored = false,
+          hide_hidden = false,
         },
       },
     },
   },
 
-  -- Telescope: Show hidden files in file finder
+  -- Snacks.nvim: Show hidden files in file picker (leader+leader)
+  -- LazyVim 14.x uses snacks by default instead of telescope
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+
+  -- Telescope: Show hidden files (fallback if used)
   {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
-        file_ignore_patterns = { ".git/" },
+        file_ignore_patterns = { "^.git/" },
       },
       pickers = {
         find_files = {
           hidden = true,
+        },
+        live_grep = {
+          additional_args = { "--hidden" },
         },
       },
     },
